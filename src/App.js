8b40f08611395e0ac7dd817a3ca7d296cdeba54c
Zack Vera <qphys50@gmail.com>
2024-10-15 17:10:18 -0700

    picking a random element and list building in react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+console.log(getRandomAnimal());
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
