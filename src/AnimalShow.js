6a8146c9b441561974f17e74154fc3b37595c951
Zack Vera <qphys50@gmail.com>
2024-10-15 22:05:17 -0700

    all styling for now

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,3 +1,4 @@
+import "./AnimalShow.css";
 import { useState } from "react";
 import bird from "./svg/bird.svg";
 import cat from "./svg/cat.svg";
@@ -23,9 +24,14 @@ function AnimalShow({ type }) {
   };
 
   return (
-    <div onClick={handleClick}>
-      <img src={svgMap[type]} alt="animal" />
-      <img src={heart} alt="heart" style={{ width: 10 + 10 * clicks + "px" }} />
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" src={svgMap[type]} alt="animal" />
+      <img
+        className="heart"
+        src={heart}
+        alt="heart"
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
     </div>
   );
 }
